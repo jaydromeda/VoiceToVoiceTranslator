c288f0d74310d6aae5167404ada86e72bcb6ff3a
jaydromeda <jeremiahdillard@yahoo.com>
2025-05-11 22:29:39 -0500

    working version

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def main():
 
 		refresh_button.click(fn=refresh_dropdowns, outputs=[rvc_dropdown, voice_dropdown])
 
-	interface.launch(server_name="0.0.0.0", server_port=5000, quiet=True)
+	interface.launch(server_name="127.0.0.1", server_port=5000, quiet=True)
 
 # delete later
 
